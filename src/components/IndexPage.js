0a5284e3edcc7506514074daa7a39c9a2a361de5
ajr-zimmer <ajr.zimmer@gmail.com>
2017-02-22 12:28:25 -0700

    cehcking to see if this fixd fetch errors.....again

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { Button, ButtonToolbar, Glyphicon } from 'react-bootstrap';
-import fetch from 'whatwg-fetch';
+import 'whatwg-fetch';
 
 export default class IndexPage extends React.Component {
 
