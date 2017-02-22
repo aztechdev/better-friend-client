91b65dc1b5287897ae4d86bd7473ec466e819675
ajr-zimmer <ajr.zimmer@gmail.com>
2017-02-22 13:03:47 -0700

    bb plssss work

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -12,6 +12,7 @@ export default class IndexPage extends React.Component {
     this.state = {
       name: ""
     };
+    this.componentWillMount = this.componentWillMount.bind(this);
   }
 
   componentWillMount(){
