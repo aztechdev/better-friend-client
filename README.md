ae60b3c70371b728127965db498eba40e8a298a2
ajr-zimmer <ajr.zimmer@gmail.com>
2017-02-21 20:01:43 -0700

    Trimmed webpack config and used current stable version of webpack

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # fb-auto
-## Generating bundle.js with webpack
-Enter the following command:
-`NODE_ENV=production node_modules/.bin/webpack -p`
-## Routing and rendering using Express
-Enter the following
-`NODE_ENV=production node_modules/.bin/babel-node --presets 'react,es2015' src/server.js`
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 "use strict";
 
-const debug = process.env.NODE_ENV !== "production";
-
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  devtool: debug ? 'inline-sourcemap' : null,
   entry: path.join(__dirname, 'src', 'app-client.js'),
   devServer: {
     inline: true,
@@ -24,25 +21,11 @@ module.exports = {
   module: {
     loaders: [{
       test: path.join(__dirname, 'src'),
-      loader: ['babel-loader'],
+      loader: 'babel-loader',
       query: {
         cacheDirectory: 'babel_cache',
-        presets: debug ? ['react', 'es2015', 'react-hmre'] : ['react', 'es2015']
+        presets: ['react', 'es2015']
       }
     }]
-  },
-  plugins: debug ? [] : [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      mangle: true,
-      sourcemap: false,
-      beautify: false,
-      dead_code: true
-    }),
-  ]
+  }
 };
