128777c4f72b680069c4cfef67b9f5ac992e5a21
ajr-zimmer <ajr.zimmer@gmail.com>
2017-02-21 17:38:38 -0700

    massive commit containing styles, layout components, routing. Need to modify server.js

---
+++ src/components/IndexPage.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+export default class IndexPage extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <p>Yoyo wudup</p>
+      </div>
+    );
+  }
+}
