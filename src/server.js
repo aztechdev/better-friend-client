ee44af51338d567d1dc982d0820282f095fec9a1
Jan Clarin <hi@janclarin.com>
2017-02-21 20:04:55 -0700

    Change default env to development

--- src/server.js
+++ src/server.js
@@ -53,7 +53,7 @@ app.get('*', (req, res) => {
 
 // start the server
 const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 server.listen(port, err => {
   if (err) {
     return console.error(err);
